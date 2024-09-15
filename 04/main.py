080bb4a7e4eb3ff293f46951ceb8ae62d891ae28
sara <sarabanann@gmail.com>
2024-09-15 19:25:15 +0330

    vowel check

---
+++ 04/main.py
@@ -0,0 +1,10 @@
+
+x = input('your character: ')
+def is_vowel(x):
+    vowel_chars = 'aeiou'
+    if x in vowel_chars:
+        print('True')
+    else:
+        print('False! not vowel!') 
+
+is_vowel(x)
