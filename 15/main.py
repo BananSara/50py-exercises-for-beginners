72f011eccaed02c21fb402e0fdf7132f17c4b067
sara <sarabanann@gmail.com>
2024-09-18 20:03:26 +0330

    find longest word

---
+++ 15/main.py
@@ -0,0 +1,13 @@
+def find_longest_word(word):
+    longest = word[0]
+    for i in word:
+        len(word[0]) == longest
+        if len(i) > len(longest):
+            longest = i
+    print(longest)
+    return len(longest)
+
+
+print(find_longest_word(['apple','orange','cat']))
+
+
