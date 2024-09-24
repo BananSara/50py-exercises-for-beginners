0c7452b1464d0c75cd2ddfaf4e1db7f92ea0b8f7
sara <sarabanann@gmail.com>
2024-09-24 16:56:04 +0330

    character frequency list

---
+++ 21/main.py
@@ -0,0 +1,13 @@
+def char_freq(chars):
+    freq = {}
+    for char in chars:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+
+print(char_freq('abbabcbdbabdbdbabababcbcbab'))
+        
+        
+    
