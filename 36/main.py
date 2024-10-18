bd767f780b340b3c8ec3f7c7fd69ab3f612d867d
sara <sarabanann@gmail.com>
2024-10-18 21:01:55 +0330

    hapaxes words

---
+++ 36/main.py
@@ -0,0 +1,23 @@
+def hapaxes():
+    file_path = input('enter your file name: ')
+    # file_path = r"C:\Users\Sara\Desktop\50py exercises for beginners\36\36.txt"
+    with open(file_path, 'r') as file:
+        words = file.read().lower().split()
+    
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1  
+        else:
+            word_counts[word] = 1  
+
+    hapaxes = []
+    for word, count in word_counts.items():
+        if count == 1:
+            hapaxes.append(word)
+            
+    return hapaxes 
+print(hapaxes())
+
+
+# bbin mn print gozshtm vali koln hichi khuruji nmide yni engar koln irad dare
